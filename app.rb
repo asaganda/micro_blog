ab6134587232bccf79de68445ea4b7b6d60c18e7
Andrew <Asaganda@gmail.com>
2015-07-02 12:11:14 -0400

    Put the sign up and sign in forms on the same .erb, added the function to be able to save a new user in the app.rb and it works via a hash. Also just added the feature to enable sessions

--- app.rb
+++ app.rb
@@ -5,23 +5,33 @@ require 'sinatra/reloader'
 
 set :database, "sqlite3:bookface.sqlite3"
 
-get '/sign-up' do
+enable :sessions
+
+get '/' do
   erb :sign_up
-  
 end
 
-post '/sign_up' do
-  redirect '/home'
+# get '/signup' do
+#   erb :sign_up
+# end
+
+post '/signup' do
+  User.create :username => params[:username], :email => params[:email], :password => params[:password]
+
+  redirect '/profile'
   #add this as a flash notice later
   # "You've Signed Up!"
-
 end
 
 post '/sign_in' do
-  "Welcome Back!"
+  redirect '/home'
 end
 
 get '/home' do
+  "Welcome back your feed!"
+end
 
+get '/profile' do
+  "Welcome to your profile"
 end
 
