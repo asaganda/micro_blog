d6945dc7afd460cd19981426b553804b66a5716a
Andrew <Asaganda@gmail.com>
2015-07-02 14:03:34 -0400

    Add configure line of code to app.rb for heroku purpose

--- app.rb
+++ app.rb
@@ -7,6 +7,8 @@ set :database, "sqlite3:bookface.sqlite3"
 
 enable :sessions
 
+configure(:development){set :database, "sqlite3:bookface.sqlite3"}
+
 get '/' do
   erb :sign_up
 end
@@ -32,6 +34,12 @@ get '/home' do
 end
 
 get '/profile' do
-  "Welcome to your profile"
+    
 end
 
+get '/signout' do
+
+  session[:user_id] = nil
+  flash[:notice] = "Signed Out Successfully. Come back soon!"
+  redirect '/'
+end
