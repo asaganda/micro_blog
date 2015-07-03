00de091d97edfc036ba5a504eb258b6cbfcffa73
Andrew <Asaganda@gmail.com>
2015-07-03 16:21:38 -0400

    Added the functionality to the profile.erb where whoever is signed in, their username will show in the Name field via an instance variable in app.rb under the '/profile' route and the @ tag in the profile.erb itself

--- app.rb
+++ app.rb
@@ -16,6 +16,7 @@ def current_user
   if session[:user_id]
     User.find session[:user_id]
   end
+
 end
 
 
@@ -56,6 +57,7 @@ get '/home' do
 end
 
 get '/profile' do
+    @user = User.find session[:user_id]
     erb :profile
 end
 
